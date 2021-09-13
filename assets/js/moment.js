4f3512a9cf755241b16f1997b9456326d932fc7b
jareddodson <jared.dodson04@gmail.com>
2021-09-12 22:38:00 -0500

    Added current date to webpage

--- assets/js/moment.js
+++ assets/js/moment.js
@@ -1,10 +1,7 @@
 var tasks = {};
 
-moment().toDate();
-
-
 var createTask = function(taskText, taskDate, taskList) {
-    // create elements that make up a task item
+
     var taskLi = $("<li>").addClass("list-group-item");
     var taskSpan = $("<span>")
       .addClass("badge badge-primary badge-pill")
@@ -13,27 +10,22 @@ var createTask = function(taskText, taskDate, taskList) {
       .addClass("m-1")
       .text(taskText);
   
-    // append span and p element to parent li
     taskLi.append(taskSpan, taskP);
   
   
-    // append to ul list on the page
     $("#list-" + taskList).append(taskLi);
   };
   
   var loadTasks = function() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
   
-    // loop over object properties
     $.each(tasks, function(list, arr) {
       console.log(list, arr);
-      // then loop over sub-array
       arr.forEach(function(task) {
         createTask(task.text, task.date, list);
       });
     });
 };
 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
+
+
